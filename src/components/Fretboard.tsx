424e4e171bed5a488814f86e948ffc916126af73
AlasdairWallaceMackie <73504675+AlasdairWallaceMackie@users.noreply.github.com>
2023-02-19 14:45:03 -0500

    String components now based on currentTuning state

--- src/components/Fretboard.tsx
+++ src/components/Fretboard.tsx
@@ -2,16 +2,17 @@ import React from "react"
 
 import String from "./String"
 import { noteCodeLookup } from "../data/notes"
+import tunings from "../data/tunings"
 
 export default function Fretboard(){
+    const [currentTuning, setCurrentTuning] = React.useState(tunings[0])
+    const stringElements = currentTuning.notes.map((note, index) => (
+        <String key={index} pitch={note}/>
+    ))
+
     return (
         <div id="fretboard">
-            <String pitch={noteCodeLookup['E']} numberOfFrets={24}/>
-            <String pitch={noteCodeLookup['B']} numberOfFrets={24}/>
-            <String pitch={noteCodeLookup['G']} numberOfFrets={24}/>
-            <String pitch={noteCodeLookup['D']} numberOfFrets={24}/>
-            <String pitch={noteCodeLookup['A']} numberOfFrets={24}/>
-            <String pitch={noteCodeLookup['E']} numberOfFrets={24}/>
+            {stringElements}
         </div>
     )
 }
