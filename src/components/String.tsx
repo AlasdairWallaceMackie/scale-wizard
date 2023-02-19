51330cc59c21ec92d105f72648b9335df0c6af5f
AlasdairWallaceMackie <73504675+AlasdairWallaceMackie@users.noreply.github.com>
2023-02-19 14:48:57 -0500

    Improvement on Fret component generation

--- src/components/String.tsx
+++ src/components/String.tsx
@@ -2,32 +2,46 @@ import React from "react"
 
 import Fret from "./Fret" //Change this to 'Fret'
 
-import { MAX_NUMBER_OF_NOTES, noteLookup } from "../data/notes"
+import { MAX_NUMBER_OF_NOTES, noteCodeLookup, flatToSharpConversion } from "../data/notes"
 
 type Props = {
-    pitch: number
+    pitch: string
     numberOfFrets: number
 }
 
 export default function String(props: Props){
-    
-    let frets: number[] = []
-    let newValue: number = 0
+    let pitchNote = props.pitch.slice(0,-1)
+    let octave = parseInt(props.pitch.slice(-1))
+
+    if (/\d/.test(pitchNote))
+        console.error("String octave is too high of a value (>9)")
+    if (/[b]/.test(pitchNote))
+        pitchNote = flatToSharpConversion[pitchNote]
+
+    let frets: {pitch: number, octave: number}[] = []
+    let fretPitch: number = noteCodeLookup[pitchNote]
     for (var i=1; i<=props.numberOfFrets; i++){
-        newValue = props.pitch + i
-        while (newValue > MAX_NUMBER_OF_NOTES)
-            newValue -= MAX_NUMBER_OF_NOTES
-        frets.push(newValue)
+        fretPitch++
+        if (fretPitch > MAX_NUMBER_OF_NOTES){
+            fretPitch = 1
+            octave++
+        }
+        frets.push({pitch: fretPitch, octave: octave})
     }
-    const fretElements = frets.map(fret => (
-        <Fret pitch={fret}/>
+
+    const fretElements = frets.map(({pitch, octave}) => (
+        <Fret pitchCode={pitch} octave={octave}/>
     ))
 
 
     return (
         <div className="string">
-            <strong>{noteLookup[props.pitch]} |</strong>
+            <strong>{pitchNote}|&nbsp;</strong>
             {fretElements}
         </div>
     )
+}
+
+String.defaultProps = {
+    numberOfFrets: 24,
 }
