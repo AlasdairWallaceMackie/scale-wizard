cf644bcb327c41127b4da1f4a15812a073653d5d
AlasdairWallaceMackie <73504675+AlasdairWallaceMackie@users.noreply.github.com>
2023-02-19 14:53:14 -0500

    Improvements

--- src/data/notes.ts
+++ src/data/notes.ts
@@ -1,25 +1,26 @@
-export const MAX_NUMBER_OF_NOTES: number = 12
-
-export const noteLookup: {[key: number]: string} = {
-    1: "A",
-    2: "A#",
-    3: "B",
-    4: "C",
-    5: "C#",
-    6: "D",
-    7: "D#",
-    8: "E",
-    9: "F",
-    10: "F#",
-    11: "G",
-    12: "G#",
-}
-
 const flip = (data: object) => Object.fromEntries(
     Object
         .entries(data)
         .map(([key, value]) => [value, parseInt(key)])
     );
+
+export const MAX_NUMBER_OF_NOTES: number = 12
+
+export const noteLookup: {[key: number]: string} = {
+    1: "C",
+    2: "C#",
+    3: "D",
+    4: "D#",
+    5: "E",
+    6: "F",
+    7: "F#",
+    8: "G",
+    9: "G#",
+    10: "A",
+    11: "A#",
+    12: "B",
+}
+
 export const noteCodeLookup = flip(noteLookup)
 
 export const sharpToFlatConversion = {
@@ -29,3 +30,4 @@ export const sharpToFlatConversion = {
     "F#": "Gb",
     "G#": "Ab",
 }
+export const flatToSharpConversion = flip(sharpToFlatConversion)
