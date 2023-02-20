3e0cb937f18f0a09d13af388a05e9c79d2c52e6a
AlasdairWallaceMackie <73504675+AlasdairWallaceMackie@users.noreply.github.com>
2023-02-20 18:11:11 -0500

    Add numberOfFrets to state

--- src/App.tsx
+++ src/App.tsx
@@ -3,11 +3,12 @@ import React from "react"
 import Fretboard from "./components/Fretboard"
 
 export default function App(){
+    const [numberOfFrets, setNumberOfFrets] = React.useState(24)
+
     return(
         <main className="p-3">
             <h1 className="text-center">Scale Wizard</h1>
-
-            <Fretboard />
+            <Fretboard numberOfFrets={numberOfFrets}/>
         </main>
     )
 }
