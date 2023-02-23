4152d9226f1d4802565640b76fd7a62533c394dc
AlasdairWallaceMackie <73504675+AlasdairWallaceMackie@users.noreply.github.com>
2023-02-23 12:23:06 -0500

    Prep for position feature using props.inPosition

--- src/components/Fret.tsx
+++ src/components/Fret.tsx
@@ -1,31 +1,37 @@
 import React from "react"
 
 import { ControlsContext } from "../context/controlsContext"
-import { noteDisplay } from "../data/notes"
+import { Pitch, noteDisplay } from "../data/notes"
+import { DEFAULT } from "../data/settings"
 
 type Props = {
-    pitchCode: number,
-    octave: number,
+    pitch: Pitch,
     openFret: boolean
+    inPosition: boolean,
 }
 
 export default function Fret(props: Props){
     const context = React.useContext(ControlsContext)
 
-    const pitchName = noteDisplay(props.pitchCode, context.sharpOrFlat.value)
+    const pitchName = 
+        noteDisplay(props.pitch.note, context.sharpOrFlat.value) + 
+            (DEFAULT.SHOW_OCTAVE_NUMBERS === true ? 
+                props.pitch.octave.toString()
+                : ""
+            )
     
     const dynamicClassName = props.openFret ? "open-fret" : ""
 
-    let noteIsVisible = true
+    let noteNameVisible = true
     let scaleMarkerColorClass = ""
 
-    if (props.pitchCode === context.currentKey.value )
+    if (props.pitch.note === context.currentKey.value )
         scaleMarkerColorClass = "bg-danger"
-    else if (context.scaleDegrees.value.includes(props.pitchCode))
+    else if (context.currentScaleDegrees.value.includes(props.pitch.note))
         scaleMarkerColorClass = "bg-info"
     else {
         scaleMarkerColorClass = "bg-transparent"
-        noteIsVisible = false
+        noteNameVisible = false
     }
 
 
@@ -33,15 +39,19 @@ export default function Fret(props: Props){
     return(
         <div className={`fret col p-0 user-select-none ${dynamicClassName}`}>
             <div className="string-hr"></div>
-            <div className={`scale-marker ${scaleMarkerColorClass}`}>
-                {
-                    context.showNoteNames.value === true ?
-                        <div className="note-name text-center">
-                            {noteIsVisible ? pitchName: <></>}
-                        </div> :
-                        <></>
+            {
+                (props.inPosition === true || context.showAllPositions.value === true) ?
+                <div className={`scale-marker ${scaleMarkerColorClass}`}>
+                    {
+                        context.showNoteNames.value === true ?
+                            <div className="note-name text-center">
+                                {noteNameVisible ? pitchName: <></>}
+                            </div> :
+                            <></>
+                    }
+                </div> :
+                <></>
                 }
-            </div>
         </div>
     )
 }
