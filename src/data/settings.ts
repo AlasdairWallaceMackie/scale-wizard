0035efcd6573a2046e7dd7b02a6c9663f236c542
AlasdairWallaceMackie <73504675+AlasdairWallaceMackie@users.noreply.github.com>
2023-02-25 09:36:03 -0500

    Adjust defaults

--- src/data/settings.ts
+++ src/data/settings.ts
@@ -4,9 +4,9 @@ export const DEFAULT: Default = {
     TUNING: "Standard",
     SHARP_FLAT: "sharp",
     KEY: "E",
-    SCALE: "Major",
+    SCALE: "Minor",
     NUMBER_OF_FRETS: 24,
-    SHOW_ALL_POSITIONS: true,
+    SHOW_ALL_POSITIONS: false,
     SHOW_NOTE_NAMES: false,
     SHOW_OCTAVE_NUMBERS: false,
 }
