3aa7ebe085382ba74fcacb1d7a417dfdd640a443
AlasdairWallaceMackie <73504675+AlasdairWallaceMackie@users.noreply.github.com>
2023-02-27 12:21:29 -0500

    Add arrow buttons for PositionRange

--- src/components/PositionRange.tsx
+++ src/components/PositionRange.tsx
@@ -6,14 +6,34 @@ export default function PositionRange(){
     const context = React.useContext(ControlsContext)
 
     return (
-        <input
-            type="range"
-            className="form-range"
-            min={0}
-            max={context.allPositions.value.length - 1}
-            value={context.currentPositionIndex.value}
-            onChange={context.currentPositionIndex.handler}
-            disabled={context.showAllNotes.value}
-        />
+        <div className="d-flex align-items-center">
+            <button
+                type="button"
+                className="btn btn-sm btn-secondary" 
+                disabled={context.showAllNotes.value}
+                onClick={() => context.currentPositionIndex.increment(-1)}
+            >
+                {"<"}
+            </button>
+
+            <input
+                type="range"
+                className="form-range mx-3"
+                min={0}
+                max={context.allPositions.value.length - 1}
+                value={context.currentPositionIndex.value}
+                onChange={context.currentPositionIndex.handler}
+                disabled={context.showAllNotes.value}
+            />
+
+            <button
+                type="button"
+                className="btn btn-sm btn-secondary" 
+                disabled={context.showAllNotes.value}
+                onClick={() => context.currentPositionIndex.increment(1)}
+            >
+                {">"}
+            </button>
+        </div>
     )
 }
